f0aa77fe9ccefe5f6b170373e38e8892d7629a30
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-09-20 17:36:25 +0000

    :wrench: Update configuration of frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    host: "0.0.0.0",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://backend:3000/graphql",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   test: {
     globals: true,
